b7c240cc743b989a8f29bdef1ca1ce4f178a5a94
Juliana Prado <80326793+julevi@users.noreply.github.com>
2022-04-29 12:58:17 -0300

    Add files via upload

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LivrariaNoctua.Controllers;
+using LivrariaNoctua.Models;
+
+namespace LivrariaNoctua.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private readonly ContextoLivros _context;
+
+        public LivrosController(ContextoLivros context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Livros.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+
+            return View(livros);
+        }
+
+  
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Autor,Editora")] Livros livros)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(livros);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livros);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros.FindAsync(id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+            return View(livros);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Autor,Editora")] Livros livros)
+        {
+            if (id != livros.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livros);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LivrosExists(livros.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livros);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+
+            return View(livros);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var livros = await _context.Livros.FindAsync(id);
+            _context.Livros.Remove(livros);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LivrosExists(int id)
+        {
+            return _context.Livros.Any(e => e.Id == id);
+        }
+    }
+}
